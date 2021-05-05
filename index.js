4b829665ddb24b3ab2028d084978b3596ccddf7d
Thiago Sousa <thiagosvin@hotmail.com>
2021-05-05 19:24:21 -0300

    Cadastrando um atendimento
    - Utilizando consign e body-parser

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const express = require('express')
+const customExpress = require("./config/customExpress");
 
-const app = express()
+const app = customExpress();
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
-
-app.get('/', (req,res) => res.send('Server rodando :D') )
-app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos e está realizando um get') )
+app.listen(3000, () => console.log("servidor rodando na porta 3000"));
