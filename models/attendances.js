9cc4ea298dca76c6fc5309d428b58c344ef5fd2b
Thiago Sousa <thiagosvin@hotmail.com>
2021-06-02 17:05:40 -0300

    Finalizando nossa API

--- models/attendances.js
+++ models/attendances.js
@@ -36,12 +36,61 @@ class Attendance {
         if (erro) {
           res.status(400).json(erro);
         } else {
-          res.status(201).json(results);
+          res.status(201).json(attendance);
       }
     });
     }
 
     
+  }
+  
+  list(res){
+    const sql = 'SELECT * FROM Attendance'
+
+    connection.query(sql, (erro, results) =>{
+      if (erro) {
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json(results)
+      }
+    })
+  }
+
+  searchById(id, res){
+    const sql = `SELECT * FROM Attendance WHERE id=${id}`
+    connection.query(sql, (erro, results) =>{
+      const attendance = results[0]
+      if (erro) {
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json(attendance)
+      }
+    })
+  }
+
+  alter(id, values, res){
+    if (values.date) {
+      values.date = moment(values.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    }
+    const sql = 'UPDATE Attendance SET ? WHERE id= ?'
+    connection.query(sql, [values, id], (erro, results) =>{
+      if (erro) {
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json({...values, id})
+      }
+    })
+  }
+
+  delete(id, res){
+    const sql = 'DELETE FROM Attendance WHERE id=?'
+    connection.query(sql, id, (erro, results) =>{
+      if (erro) {
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json({id})
+      }
+    })
   }
 }
 module.exports = new Attendance();
